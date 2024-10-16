8ed759ae2f1b095ebd08ee1824b3f61cea61f646
edmar.james <edmarjamesbautista@gmail.com>
2024-10-16 18:15:14 +0800

    Create new model for ForeignKey

--- inventory/models.py
+++ inventory/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
@@ -9,8 +18,12 @@ class Product(models.Model):
     url = models.SlugField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
     quantity = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
     # To set a verbose name for a field you can do this
     # name = models.CharField("brand_name", max_length=100)
+
+    def __str__(self):
+        return f"{self.name} - {self.category.name}"
 
 class Brand(models.Model):
     # You can explicitly define your primary key like this. See example below.
