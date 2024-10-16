86cba8fb09f0ffa4a06cbce32f9d338ec903623f
edmar.james <edmarjamesbautista@gmail.com>
2024-10-16 17:01:00 +0800

    Add new columns to Product Model

--- inventory/models.py
+++ inventory/models.py
@@ -3,6 +3,11 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    url = models.SlugField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
     quantity = models.IntegerField()
     # To set a verbose name for a field you can do this
     # name = models.CharField("brand_name", max_length=100)
