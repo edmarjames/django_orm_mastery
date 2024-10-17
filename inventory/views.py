2e305bd77eff9befdb078658b8fa14a6a8451af1
edmar.james <edmarjamesbautista@gmail.com>
2024-10-17 17:26:10 +0800

    Create new function for Pretty print raw SQL query

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Category
+
+from pygments import highlight
+from pygments.formatters import TerminalFormatter
+from pygments.lexers import MySqlLexer
+from sqlparse import format
+
 
 # Create your views here.
+def get_categories(request):
+
+    all_category = Category.objects.all()
+    sql_formatted = format(str(all_category.query), reindent=True)
+    print(highlight(sql_formatted, MySqlLexer(), TerminalFormatter()))
+
+    return HttpResponse(all_category.query)
