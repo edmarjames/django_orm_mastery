8a8356f05d2193a52828c037992e970c7f9570c3
edmar.james <edmarjamesbautista@gmail.com>
2024-10-23 14:49:44 +0800

    Create a signal and connect it with a model

--- inventory/apps.py
+++ inventory/apps.py
@@ -1,13 +1,9 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_save
 
-
-def example_receiver(sender, **kwargs):
-    print("Instance is saved")
 
 class InventoryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'inventory'
 
     def ready(self) -> None:
-        post_save.connect(example_receiver)
+        import inventory.signals
