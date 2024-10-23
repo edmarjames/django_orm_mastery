a3813cb3f9ca059cc20cf3bcb58f09fcbffb4148
edmar.james <edmarjamesbautista@gmail.com>
2024-10-23 15:17:09 +0800

    Replace single quotes with double quotes

--- inventory/models.py
+++ inventory/models.py
@@ -5,7 +5,7 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
 
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = "Categories"
 
     def __str__(self):
         return self.name
--- inventory/urls.py
+++ inventory/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "inventory"
 
 urlpatterns = [
-    path('', views.get_categories, name="get_categories"),
-    path('create_product', views.create_product, name="create_product"),
-    path('create_stock', views.create_stock, name="create_stock"),
+    path("", views.get_categories, name="get_categories"),
+    path("create_product", views.create_product, name="create_product"),
+    path("create_stock", views.create_stock, name="create_stock"),
 ]
